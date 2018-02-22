fee5eb9ccbf76e617413336e7dfe48111fa4193b
root <ssbdan@gmail.com>
2018-02-21 23:46:52 -0800

    tset

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
- echo "I am VERSION 2 <br><br>";
+ echo "I am VERSION 1 <br><br>";
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
+ echo "<br><br>";
 ?>
 
 Hello. Today is <?= date('l \t\h\e jS') ?>.
